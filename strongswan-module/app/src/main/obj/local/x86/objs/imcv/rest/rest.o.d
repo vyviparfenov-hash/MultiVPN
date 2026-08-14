f6700bc300aafa7cd90f44da967e7369a4d357fc
vyviparfenov-hash <vyviparfenov@gmail.com>
2026-08-15 00:46:42 +0300

    0.9.03

--- strongswan-module/app/src/main/obj/local/x86/objs/imcv/rest/rest.o.d
+++
@@ -1,2 +0,0 @@
-/home/runner/work/amnezia-simple/amnezia-simple/strongswan-src/src/frontends/android/app/src/main/obj/local/x86/objs/imcv/rest/rest.o: \
-  /home/runner/work/amnezia-simple/amnezia-simple/strongswan-src/src/frontends/android/app/src/main/jni/../../../../../../..//src/libimcv/rest/rest.c
